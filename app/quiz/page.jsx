8c1e3a24572a6fc443e165b2bba2a74a4df7fec0
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2024-01-02 15:22:56 +0330

    feat: development next question and handle feature result question

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -20,11 +20,49 @@ const Quiz = () => {
     const { questions } = quiz;
     const { answers, correctAnswer } = questions[activeQuestion];
 
+    // Select And Check
+
+    const handleAnswerSelected = (answer, index) => {
+        setChecked(true);
+        setSelectedAnswerIndex(index);
+        
+        if (answer === correctAnswer) {
+            setSelectedAnswer(true);
+            console.log("true answer");
+        } else {
+            setSelectedAnswer(false);
+            console.log("false answer");
+        }
+    }
+
+    // Calculate Score and Increment to next question
+    const handleNextQuestion = () => {
+        setSelectedAnswerIndex(null);
+        setResult(prev => selectedAnswer ? 
+            {...prev, score: prev.score + 5, correctAnswers: prev.correctAnswers + 1}
+            : 
+            {...prev, wrongAnswers: prev.wrongAnswers + 1}
+        );
+
+        if (activeQuestion !== questions.length - 1) {
+            setActiveQuestion(prev => prev +  1);
+        } else {
+            setActiveQuestion(0);
+            setShowResult(true);
+        }
+
+        setChecked(false);
+    }
 
     return (
         <main className="min-h-screen bg-indigo-900">
             <div className="lg:max-w-screen-lg m-auto">
                 <h1 className="dark:text-white text-3xl font-bold pt-10">صفحه آزمون</h1>
+                <div className="mt-8">
+                    {!showResult ? (
+                        <h2 className="text-xl font-medium text-white">آزمون: {activeQuestion + 1} از {questions.length}</h2>
+                    ) : null}
+                </div>
                 <div className="dark:bg-zinc-700 drop-shadow-2xl rounded-xl mt-10">
                     {!showResult ? (
                         <div className="p-4">
@@ -34,9 +72,9 @@ const Quiz = () => {
                                     answers.map((answer, index) => (
                                         <li 
                                             key={index} 
-                                            onClick={() => {}} 
+                                            onClick={() => handleAnswerSelected(answer, index)} 
                                             className={`${selectedAnswerIndex === index ? 
-                                                'border border-zinc-500 rounded p-2 my-4' 
+                                                'border border-zinc-500 bg-blue-600 rounded p-2 my-4' 
                                                 :
                                                 'border border-zinc-500 rounded p-2 my-4 transition-all duration-300 ease-in-out hover:bg-zinc-500'}`}
                                         >
@@ -49,15 +87,15 @@ const Quiz = () => {
                                 {
                                     checked ? (
                                         <button 
-                                            className="dark:bg-indigo-500 dark:text-white py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-indigo-700" 
-                                            onClick={() => {}}
+                                            className="dark:bg-indigo-600 dark:text-white py-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-indigo-700" 
+                                            onClick={handleNextQuestion}
                                         >
                                                 {activeQuestion === questions.length - 1 ? 'پایان' : 'بعدی'}
                                             </button>
                                         ) : (
                                             <button 
-                                                className="dark:bg-indigo-500 dark:text-white py-2 rounded-lg disabled:opacity-70" 
-                                                onClick={() => {}} 
+                                                className="dark:bg-indigo-400 dark:text-white py-2 rounded-lg disabled:opacity-60" 
+                                                onClick={handleNextQuestion} 
                                                 disabled
                                             >
                                                 {activeQuestion === questions.length - 1 ? 'پایان' : 'بعدی'}
